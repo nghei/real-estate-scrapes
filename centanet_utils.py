ad515f05b7ab568d9eba11ac74dbb6c52dfd81fa
nghei <ngheii@gmail.com>
2016-06-21 05:19:29 +0000

    Orders on market - Work in progress 2016-06-21

--- centanet_utils.py
+++ centanet_utils.py
@@ -3,16 +3,34 @@ import requests
 import json
 import lxml
 import lxml.html
+import re
 
-# Centanet
+### Utility Functions ###
+
+def get_numeric_part(s):
+    return float("".join([x for x in s if x.isdigit() or x == '.']))
+
+def get_unit(s):
+    if s == u'億':
+        return 1e8
+    elif s == u'萬':
+        return 1e4
+    else:
+        return 1
+
+def get_numeric(s):
+    return get_numeric_part(s) * get_unit(s[-1])
+
+### Centanet ###
+
+# Constants
 
 BASE_URL = "http://hk.centanet.com/findproperty/BLL/Result_SearchHandler.ashx"
 SUFFIX_URL = "http://hk.centanet.com/findproperty/zh-HK/Home/SearchResult/"
 COUNT_URL = "http://hk.centanet.com/findproperty/zh-HK/Service/GetBarChartAjax"
 COUNT_SUFFIX_URL = "http://hk.centanet.com/findproperty/zh-HK/Home/SearchResult/"
 
-MIDLAND_URL = ""
-
+# Historical transactions
 
 def get_districts(big_district=None):
     big_districts = [big_district]
@@ -29,8 +47,10 @@ def get_districts(big_district=None):
         for table in tables:
             print(table.text_content())
 
+# Orders on market
+
 def get_page(page, posttype="S"):
-    subparams = { "posttype" : posttype, "limit" : -1, "currentpage" : page }
+    subparams = { "maxprice" : 2000000, "posttype" : posttype, "limit" : -1, "currentpage" : page }
     suffix_url = "%s?%s" % (SUFFIX_URL, urllib.parse.urlencode(subparams))
     params = { "url" : suffix_url }
     req = requests.get(BASE_URL, params=params)
@@ -56,6 +76,88 @@ def get_pages(posttype="S"):
         except:
             continue
     return divs
+
+def parse_page_content(content):
+    tree = lxml.html.fromstring(content)
+    properties = tree.xpath('//div[@class="SearchResult_Row"]')
+    res = []
+    for p in properties:
+        info = {}
+        left = p.xpath("*[@class='ContentInfo_Left fLeft']")
+        if len(left) > 0:
+            left = left[0]
+        else:
+            continue
+        right = p.xpath("*[@class='ContentInfo_Right fRight']")
+        if len(right) > 0:
+            right = right[0]
+        else:
+            continue
+        header = left.xpath("//*[@class='ContentInfo_Header' and contains(@title, '')]")
+        title = header[0].get("title") if len(header) > 0 else None
+        info["title"] = title
+        ps = left.xpath("p")
+        for p in ps:
+            s = p.text_content()
+            info["district"] = s
+            break
+        sale_price = right.xpath("p/span/span")
+        sale_price = get_numeric(sale_price[0].text_content().strip()) if len(sale_price) > 0 else None
+        info["sale_price"] = sale_price
+        subinfo = left.xpath("*[@class='ContentInfo_SubInfo']")
+        for i in subinfo:
+            gross_area = None
+            net_area = None
+            gross_price = None
+            net_price = None
+            is_bo = False
+            price_sizes = i.xpath("*[@class='price_size']")
+            for ps in price_sizes:
+                sizes = ps.xpath("p")
+                for s in sizes:
+                    print(lxml.html.tostring(s))
+                    print(s.text_content())
+                    area = get_numeric_part(s.text_content())
+                    break
+                prices = ps.xpath("div/div/span")
+                ps = []
+                for p in prices:
+                    print(lxml.html.tostring(p))
+                    print(p.text_content())
+                    try:
+                         ps.append(get_numeric_part(p.text_content()))
+                    except:
+                         is_bo = True
+                try:
+                    price = max(ps)
+                except:
+                    continue
+                if net_area is None or net_price is None:
+                    net_area = area
+                    net_price = price
+                else:
+                    if area < net_area:
+                        gross_area = net_area
+                        gross_price = net_price
+                        net_area = area
+                        net_price = price
+                    else:
+                        gross_area = area
+                        gross_price = price
+            details = i.xpath("*[contains(@class, 'ContentInfo_DetailStr_Lf')]")
+            details = "|".join([d.text_content().strip() for d in details])
+            info["gross_area"] = gross_area
+            info["net_area"] = net_area
+            info["gross_price"] = gross_price
+            info["net_price"] = net_price
+            info["is_bo"] = is_bo
+            info["details"] = details
+            break
+        print(info)
+        res.append(info)
+    return res
+
+# Unused
 
 def get_midland_page(page, tx_type="S"):
     params = { "estate_name" : "", "priceFrom" : None, "priceTo" : None, "areaFrom" : None, "areaTo" : None, "bedroom" : "", "tx_type" : tx_type, "area_type" : "net_area", "is_hos" : False, "autocompleteString" : "", "districtIds" : "", "estIds" : "", "latLngBounds" : "22.261965,113.92932,22.484834,114.290496" , "page" : page, "sort" : "", "bldgIds" : "", "feature" : "", "is_random" : False }
